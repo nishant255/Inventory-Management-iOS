3194b28cd8a81f69f86ac06605586da2c6585b57
Marcus Sakoda <Jubhub@yahool.com>
2017-03-22 15:50:07 -0700

    small changes here and there

---
+++ InventoryHeaderCustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryHeaderCustomCell.swift
+//  Inventory Management
+//
+//  Created by Marcus Sakoda on 3/22/17.
+//  Copyright © 2017 Nishant Patel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InventoryHeaderCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+}
