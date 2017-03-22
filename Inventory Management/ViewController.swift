7a39ce359f0266a44855c0428108e1d8d7f96dd2
Nishant Patel <contact@nishant-patel.com>
2017-03-21 18:40:21 -0700

    Initial Commit

---
+++ Inventory Management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inventory Management
+//
+//  Created by Nishant Patel on 3/21/17.
+//  Copyright © 2017 Nishant Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
