bd1a5a620788f621fd34512ecfb1c74195329a7e
Marcus Sakoda <Jubhub@yahool.com>
2017-03-22 19:42:58 -0700

    worked on users

--- Inventory Management/CompaniesViewController.swift
+++ Inventory Management/CompaniesViewController.swift
@@ -19,6 +19,53 @@ class CompaniesViewController: UIViewController, BackButtonDelegate {
         companiesTableView.dataSource = self
         companiesTableView.delegate = self
         print("companiesviewcontroller loaded")
+        
+        let host = "http://localhost:8000/"
+        
+        
+        let url = NSURL(string: host+"companies")
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url! as URL, completionHandler: {
+            data, response, error in
+            do {
+                print("in the do")
+                
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSArray {
+                    
+                    
+                    print("result is =====>",jsonResult)
+                    
+                    print(jsonResult.count)
+                    for var i in 0..<jsonResult.count {
+                        let company = jsonResult[i] as! NSDictionary
+                        let id = String(describing: company["_id"]!)
+                        let name = company["name"] as! String
+                        let email = company["email"] as! String
+                        let phone = company["phone"] as! String
+                        let addressDict = company["address"] as! NSDictionary
+                        var address = ""
+                        address += "\(addressDict["street"]!), \(addressDict["city"]!), \(addressDict["state"]!), \(addressDict["zipcode"]!)"
+                        let itemArray = [name,email,phone,address,id]
+                        self.companies.append(itemArray)
+                        DispatchQueue.main.async {
+                            self.companiesTableView.reloadData()
+                        }
+                    }
+                }
+            } catch {
+                print("in the catch")
+                print(error)
+            }
+        })
+        task.resume()
+        print("I happen before the response!")
+        
+        
+        
+        
+        
+        
     }
     @IBOutlet weak var companiesTableView: UITableView!
 
